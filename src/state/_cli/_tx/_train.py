e3fe0cd7577abfbd73fed51b8ae367e11e576843
Tom Berry <tberry860+github@gmail.com>
2025-09-05 18:26:32 -0400

    check val less often

--- src/state/_cli/_tx/_train.py
+++ src/state/_cli/_tx/_train.py
@@ -240,6 +240,7 @@ def run_tx_train(cfg: DictConfig):
         accelerator=accelerator,
         devices=1,
         max_epochs=cfg["training"].get("max_epochs", 500),  # for normal models
+        check_val_every_n_epochs=8,
         logger=loggers,
         plugins=plugins,
         callbacks=callbacks,
