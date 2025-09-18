140acf204798da68ec3f3dcdefd05216e7776a00
Tom Berry <tberry860+github@gmail.com>
2025-09-18 09:35:07 -0400

    don't unnecessarily create dataloader

--- src/state/_cli/_tx/_train.py
+++ src/state/_cli/_tx/_train.py
@@ -122,9 +122,6 @@ def run_tx_train(cfg: DictConfig):
         data_module.save_state(f)
 
     data_module.setup(stage="fit")
-    dl = data_module.train_dataloader()
-    print("num_workers:", dl.num_workers)
-    print("batch size:", dl.batch_size or data_module.batch_size)
 
     var_dims = data_module.get_var_dims()  # {"gene_dim": …, "hvg_dim": …}
     if cfg["data"]["kwargs"]["output_space"] == "gene":
