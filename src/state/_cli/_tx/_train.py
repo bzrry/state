216b33ce3e227bd8c5526a432e393077d2eddf86
Tom Berry <tberry860+github@gmail.com>
2025-09-18 07:31:25 -0400

    don't sanity check to avoid caching batches twice

--- src/state/_cli/_tx/_train.py
+++ src/state/_cli/_tx/_train.py
@@ -258,6 +258,7 @@ def run_tx_train(cfg: DictConfig):
         callbacks=callbacks,
         gradient_clip_val=cfg["training"]["gradient_clip_val"] if cfg["model"]["name"].lower() != "cpa" else None,
         precision="bf16-mixed",
+        num_sanity_val_steps=0,
     )
 
     # Align logging cadence with rolling MFU window (and W&B logging)
